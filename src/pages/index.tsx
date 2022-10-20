7190e4854568fa4874335671f6881cc56ec1cb22
Dwayne Sutherland <dwayneasutherland1@gmail.com>
2022-10-20 08:02:52 +0100

    intial commit t3 boilerplate

---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Odin Book - Home</title>
+        <meta name="description" content="Odin Book - Twitter/Facebook clone by Dwayne Sutherland" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h1>home</h1>
+      </main>
+    </>
+  );
+};
+
+export default Home;
