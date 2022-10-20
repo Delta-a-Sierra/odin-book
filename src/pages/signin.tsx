57cb69bdb515061a4116dacb170eb51238412666
Dwayne Sutherland <dwayneasutherland1@gmail.com>
2022-10-20 10:12:48 +0100

    setup tailwind.conf

---
+++ src/pages/signin.tsx
@@ -0,0 +1,20 @@
+
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Signin: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Odin Book - signin</title>
+        <meta name="description" content="Odin Book - Twitter/Facebook clone by Dwayne Sutherland" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h1>signin</h1>
+      </main>
+    </>
+  );
+};
+
+export default Signin;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#084887",
+        secondary: "#909CC2",
+        accent: "#F58A07",
+        white: "#fff",
+        online: "#4ACB68",
+        offline: "#A5A5A5",
+        away: "#E2C639",
+        busy: "#E25313",
+        gray: {
+          100: "#f2f2f2",
+          200: "#e9e9e9",
+          500: "#7f7f7f",
+        },
+      },
+    },
+
   },
   plugins: [],
 };
