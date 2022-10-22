141545d6ce37fa45ac9c7e1d37a2f0c0c085b3aa
Dwayne Sutherland <dwayneasutherland1@gmail.com>
2022-10-22 15:23:45 +0100

    added theme context

---
+++ src/contexts/theme.tsx
@@ -0,0 +1,50 @@
+import { createContext, Dispatch, useContext, useReducer } from "react";
+
+interface ThemeContextInterface {
+  state: ThemeState;
+  dispatch: Dispatch<ThemeAction>;
+}
+
+type ThemeProviderProps = {
+  children: string
+}
+
+type ThemeState = {
+  dark: boolean
+}
+
+type ThemeAction = {
+  type: string
+}
+
+const ThemeContext = createContext<ThemeContextInterface | undefined>(undefined);
+
+function ThemeReducer(state: ThemeState, action: ThemeAction) {
+  switch (action.type) {
+    case "toggle": {
+      return { dark: !state.dark };
+    }
+    default: {
+      return state
+    }
+  }
+}
+
+const ThemeProvider: React.FC<ThemeProviderProps> = ({ children }) => {
+  const [state, dispatch] = useReducer(ThemeReducer, { dark: false });
+
+  const value = { state, dispatch };
+  return (
+    <ThemeContext.Provider value={value}>{children}</ThemeContext.Provider>
+  );
+}
+
+function useTheme() {
+  const context = useContext(ThemeContext);
+  if (context === undefined) {
+    throw new Error("useCount must be used within a CountProvider");
+  }
+  return context;
+}
+
+export { ThemeProvider, useTheme };
